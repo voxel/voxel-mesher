0f9527240975c7c19aaf7163eb4ae56e7fef9f3a
deathcap <deathcap@gmx.co.uk>
2014-06-12 21:16:16 -0700

    Process voxel array into solidVoxels, applying opaque bit from registry
    Instead of being baked into the voxel array directly, the
    opaque/transparent bit is now added in on blocks with the 'transparent'
    property set in voxel-registry. This hides the opaque bit implementation
    detail from the voxel format, so for example, voxel-engine getBlock()
    and setBlock() on glass is now e.g., 46, instead of 32814 -- the
    opaqueness is internal to the meshing engine, not exposed outside.
    
    Ref GH-5

--- mesh-plugin.js
+++ mesh-plugin.js
@@ -3,18 +3,61 @@
 var createVoxelMesh = require('./mesh-buffer.js');
 var inherits = require('inherits');
 var EventEmitter = require('events').EventEmitter;
+var ndarray = require('ndarray');
+var ops = require('ndarray-ops');
 
 module.exports = function(game, opts) {
   return new MesherPlugin(game, opts);
 };
 module.exports.pluginInfo = {
+  loadAfter: ['voxel-registry'],
   clientOnly: true
 };
 
 function MesherPlugin(game, opts) {
   this.game = game;
+
+  this.registry = game.plugins.get('voxel-registry');
+  if (!this.registry) throw new Error('voxel-mesher requires voxel-registry plugin');
+
+  this.transparentMap = undefined;
+
+  var s = game.chunkSize + (game.chunkPad|0)
+  this.solidVoxels = ndarray(new game.arrayType(s*s*s), [s,s,s]);
 };
 inherits(MesherPlugin, EventEmitter);
 
-MesherPlugin.prototype.createVoxelMesh = createVoxelMesh;
+MesherPlugin.prototype.createVoxelMesh = function(gl, voxels, voxelSideTextureIDs, voxelSideTextureSizes, position, pad) {
+  this.splitVoxelArray(voxels);
+
+  return createVoxelMesh(gl, this.solidVoxels, voxelSideTextureIDs, voxelSideTextureSizes, position, pad);
+}
+
+// populates solidVoxels
+MesherPlugin.prototype.splitVoxelArray = function(voxels) {
+  if (!this.transparentMap) {
+    // cache list of transparent voxels TODO: refresh cache when changes
+    this.transparentMap = this.registry.getBlockPropsAll('transparent');
+    this.transparentMap.unshift(true); // air (0) is transparent
+  }
+
+  var begin = Date.now();
+  var solidVoxels = this.solidVoxels;
+  var transparentMap = this.transparentMap;
+  ops.assign(solidVoxels, voxels);
+
+  // add opaque bit as needed
+  var length = solidVoxels.data.length;
+  for (var i = 0; i < length; ++i) {
+    var value = solidVoxels.data[i];
+    if (!transparentMap[value]) {
+      value |= (1<<15);
+      solidVoxels.data[i] = value;
+    }
+  }
+  // TODO: also separate out non-solid voxels for second-phase rendering
+
+  var took = Date.now() - begin;
+  if (took > 10) console.log('splitVoxelArray '+took+' ms');
+}
 
