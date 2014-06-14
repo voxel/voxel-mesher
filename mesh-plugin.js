voxel/voxel-mesher

commit 9c9cc67995bad2f06f1e4c6de8e0ebed10afc539

Add custom block model support. Ref GH-5
Initial implementation, needs work
Ref https://github.com/deathcap/voxel-ideas/issues/3

mesh-plugin.js
@@ -5,57 +5,117 @@ var inherits = require('inherits');
 var EventEmitter = require('events').EventEmitter;
 var ndarray = require('ndarray');
 var ops = require('ndarray-ops');
+var createBlockGeometry = require("block-models");
 
 module.exports = function(game, opts) {
   return new MesherPlugin(game, opts);
 };
 module.exports.pluginInfo = {
-  loadAfter: ['voxel-registry'],
+  loadAfter: ['voxel-registry', 'voxel-stitch'],
   clientOnly: true
 };
 
 function MesherPlugin(game, opts) {
   this.game = game;
+  this.shell = game.shell;
 
   this.registry = game.plugins.get('voxel-registry');
   if (!this.registry) throw new Error('voxel-mesher requires voxel-registry plugin');
 
-  this.transparentMap = undefined;
+  this.stitcher = game.plugins.get('voxel-stitch');
+  if (!this.stitcher) throw new Error('voxel-mesher requires voxel-stitch plugin');
+
+  this.isTransparent = undefined;
+  this.hasBlockModel = undefined;
 
   var s = game.chunkSize + (game.chunkPad|0)
   this.solidVoxels = ndarray(new game.arrayType(s*s*s), [s,s,s]);
+  this.porousVoxels = []; //ndarray(new game.arrayType(s*s*s), [s,s,s]);
 };
 inherits(MesherPlugin, EventEmitter);
 
 MesherPlugin.prototype.createVoxelMesh = function(gl, voxels, voxelSideTextureIDs, voxelSideTextureSizes, position, pad) {
   this.splitVoxelArray(voxels);
 
-  return createVoxelMesh(gl, this.solidVoxels, voxelSideTextureIDs, voxelSideTextureSizes, position, pad);
+  var mesh = createVoxelMesh(gl, this.solidVoxels, voxelSideTextureIDs, voxelSideTextureSizes, position, pad);
+
+  this.meshCustomBlocks(mesh);
+
+  return mesh;
 }
 
-// populates solidVoxels
+// meshes this.porousVoxels into mesh.vertexArrayObjects.porous
+MesherPlugin.prototype.meshCustomBlocks = function(mesh) {
+  var voxels = this.porousVoxels;
+
+  mesh.vertexArrayObjects.porous = [];
+
+  for (var i = 0; i < voxels.length; ++i) {
+    var info = voxels[i];
+    var value = info.value
+
+    var model = this.registry.blockProps[value].blockModel;
+    var stitcher = this.stitcher;
+
+    var blockMesh = createBlockGeometry(
+      this.shell.gl,
+      model,
+      //getTextureUV:
+      function(name) {
+        return stitcher.getTextureUV(name); // only available when textures are ready
+      }
+    );
+    blockMesh.position = info.position;
+
+    mesh.vertexArrayObjects.porous.push(blockMesh);
+  }
+};
+
+// populates solidVoxels, porousVoxels
 MesherPlugin.prototype.splitVoxelArray = function(voxels) {
-  if (!this.transparentMap) {
+  if (!this.isTransparent) {
     // cache list of transparent voxels TODO: refresh cache when changes
-    this.transparentMap = this.registry.getBlockPropsAll('transparent');
-    this.transparentMap.unshift(true); // air (0) is transparent
+    this.isTransparent = this.registry.getBlockPropsAll('transparent');
+    this.isTransparent.unshift(true); // air (0) is transparent
+  }
+  if (!this.hasBlockModel) {
+    this.hasBlockModel = this.registry.getBlockPropsAll('blockModel');
+    this.hasBlockModel.unshift(undefined);
   }
 
   var begin = Date.now();
+
+  // phase 1: solid voxels = opaque, transparent (terrain blocks, glass, greedily meshed)
   var solidVoxels = this.solidVoxels;
-  var transparentMap = this.transparentMap;
+  var isTransparent = this.isTransparent;
   ops.assign(solidVoxels, voxels);
 
-  // add opaque bit as needed
+  // phase 2: porous voxels = translucent, custom block models (stained glass, slabs, stairs)
+  var porousVoxels = this.porousVoxels = [];
+  var hasBlockModel = this.hasBlockModel;
+
   var length = solidVoxels.data.length;
   for (var i = 0; i < length; ++i) {
     var value = solidVoxels.data[i];
-    if (!transparentMap[value]) {
-      value |= (1<<15);
-      solidVoxels.data[i] = value;
+    if (hasBlockModel[value]) {
+      solidVoxels.data[i] = 0;
+
+      // sorry
+      var o = i;
+      var z = (o % 36)-2; o = Math.floor(o / 36);
+      var y = (o % 36)-2; o = Math.floor(o / 36);
+      var x = o-2;
+      z += voxels.position[0]*32
+      y += voxels.position[1]*32
+      x += voxels.position[2]*32
+
+      // add to list for custom rendering (list not array since fewer custom
+      // model blocks are expected, less array iteration TODO: revisit)
+      porousVoxels.push({position:[x,y,z], value:value});
+    } else if (!isTransparent[value]) {
+      solidVoxels.data[i] = value | (1<<15); // opaque bit
     }
   }
-  // TODO: also separate out non-solid voxels for second-phase rendering
 
   var took = Date.now() - begin;
   if (took > 10) console.log('splitVoxelArray '+took+' ms');

commit 1e02233d39b799807bccb1e736c70e0696ff4847

Fix 'meshed' event emitting, fixes voxel-chunkborder/wireframe
Was broken by 9c9cc67995bad2f06f1e4c6de8e0ebed10afc539

mesh-buffer.js
@@ -9,7 +9,7 @@ var glm = require("gl-matrix")
 var mat4 = glm.mat4
 
 //Creates a mesh from a set of voxels
-function createVoxelMesh(gl, voxels, voxelSideTextureIDs, voxelSideTextureSizes, position, pad) {
+function createVoxelMesh(gl, voxels, voxelSideTextureIDs, voxelSideTextureSizes, position, pad, that) {
   //Create mesh
   var vert_data = createAOMesh(voxels, voxelSideTextureIDs, voxelSideTextureSizes)
   var vertexArrayObjects = {}
@@ -58,7 +58,7 @@ function createVoxelMesh(gl, voxels, voxelSideTextureIDs, voxelSideTextureSizes,
     modelMatrix: modelMatrix
   }
 
-  if (this) this.emit('meshed', result, gl, vert_data, voxels)
+  if (that) that.emit('meshed', result, gl, vert_data, voxels)
 
   return result
 }

mesh-plugin.js
@@ -39,7 +39,7 @@ inherits(MesherPlugin, EventEmitter);
 MesherPlugin.prototype.createVoxelMesh = function(gl, voxels, voxelSideTextureIDs, voxelSideTextureSizes, position, pad) {
   this.splitVoxelArray(voxels);
 
-  var mesh = createVoxelMesh(gl, this.solidVoxels, voxelSideTextureIDs, voxelSideTextureSizes, position, pad);
+  var mesh = createVoxelMesh(gl, this.solidVoxels, voxelSideTextureIDs, voxelSideTextureSizes, position, pad, this);
 
   mesh.vertexArrayObjects.porous = this.porousMeshes;
 